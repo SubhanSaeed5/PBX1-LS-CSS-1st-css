01b427e21c3e76789d5401a814abdb047e030eba
SubhanSaeed5 <136212834+SubhanSaeed5@users.noreply.github.com>
2023-06-11 07:59:12 +0500

    not for any one

---
+++ config.js
@@ -0,0 +1,42 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "45.61.168.63:30500";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "Youtube", label: "@PBX-1Roleplay", icon: "assets/media/icons/Youtube.png"},
+    {name: "discord", label: "discord.gg/pbx1rp", icon: "assets/media/icons/discord.png"},
+    {name: "instagram", label: "@teampunjabimods", icon: "assets/media/icons/tiktok.png"},
+    
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "「Pᴜɴᴊᴀʙɪ Bᴏʏᴢ」", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1088016338292846652/1117021308241330266/PUNJABI-LANTI.jpg"},
+    {name: "「XᴛʀᴇᴍᴇDᴀDᴀ」",description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1088016338292846652/1117030716128116767/360_F_345520698_MFPB7vvQboZMowIOq3ZO9UkWNm5Il0Ny.png"},
+
+    {name: "「Fᴀʀᴅᴀɴ」", description: "Founder", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1088016338292846652/1117061080305385602/EEAF25D2-06C0-43BE-880D-479398E70AB3.png"},
+    {name: "「Tᴇᴀᴍ ɪɢX」",description: "DEV", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1088016338292846652/1117023859787440218/b6816e6170ee5ee452f948e390df4bf5.png"},
+    {name: "「Sᴜʙʜᴀɴ Sᴀᴇᴇᴅ」",description: "Staff", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1088016338292846652/1117053703598387230/07.jpg"},
+    {name: "「Zᴀɪɴ」",description: "Staff", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1088016338292846652/1117039771227799703/IMG-20230308-WA0000.jpg"},
+    {name: "「Dʜx Gᴀᴍɪɴɢ」",description: "Staff", color: "#ff0000", image: "https://yt3.googleusercontent.com/QYmTokmViruF_6YSbVjAuMUlFpPg18CAVMk-QWJ8k0_bmEedYh6KYnL-6MXZVE0IZyfwd_Qx0g=s176-c-k-c0x00ffffff-no-rj"},
+        {name: "「Sʜᴀʏᴀɴ」",description: "MUFT KHORA", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1088016338292846652/1117070433376813106/c871945cc8b39643f09c15d318506bf8.png"},
+  
+   
+ 
+    
+];
+
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: false},
+    {label: "Staff", default: true}
+    
+    
+];
+
+// Music
+ConfigSong = "songmp3";
